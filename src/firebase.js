b27a7a3452dafe16ff8fc39bd7a42197c5c89076
saurabh3122 <saurabhpriyadarshi007@gmail.com>
2022-03-04 17:38:21 +0530

    firebase updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
 
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBKM_qJm7k2kHhSA1GbGEXHNzQc6E8wfmQ",
-    authDomain: "instagram-clone-react-70113.firebaseapp.com",
-    databaseURL: "https://instagram-clone-react-70113.firebaseio.com",
-    projectId: "instagram-clone-react-70113",
-    storageBucket: "instagram-clone-react-70113.appspot.com",
-    messagingSenderId: "375283177912",
-    appId: "1:375283177912:web:ae590d1e7bf10a800b574d",
-    measurementId: "G-GR8GZJQZ3L"
-
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyBQ2i5IPyfwcJVCqebjhhBv1F7UVhe2mnk",
+    authDomain: "image-share-ea21c.firebaseapp.com",
+    databaseURL: "https://image-share-ea21c-default-rtdb.firebaseio.com",
+    projectId: "image-share-ea21c",
+    storageBucket: "image-share-ea21c.appspot.com",
+    messagingSenderId: "953601279994",
+    appId: "1:953601279994:web:addeb546295da6bab48643",
+    measurementId: "G-XJ4QHFWCTN"
+  };
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
